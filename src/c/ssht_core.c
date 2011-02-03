7d9e8339de5b18bffdf7eb9dcd6023dc10340774
Jason McEwen <jason.mcewen@ucl.ac.uk>
2011-02-03 20:03:05 +0000

    Tidied MW real forward and inverse algorithms for symmetric sampling.

--- src/c/ssht_core.c
+++ src/c/ssht_core.c
@@ -43,7 +43,6 @@ void ssht_core_mw_forward_sov_conv_sym_ss_real(complex double *flm, double *f,
   int dl_offset, dl_stride;
   int w_offset;
   complex double *expsm;
-//, *expsmm;
   int exps_offset;
   int elmmsign, elssign;
   int spinneg;
@@ -56,8 +55,6 @@ void ssht_core_mw_forward_sov_conv_sym_ss_real(complex double *flm, double *f,
   SSHT_ERROR_MEM_ALLOC_CHECK(signs)
   expsm = (complex double*)calloc(L, sizeof(complex double));
   SSHT_ERROR_MEM_ALLOC_CHECK(expsm)
-//  expsmm = (complex double*)calloc(2*L-1, sizeof(complex double));
-//  SSHT_ERROR_MEM_ALLOC_CHECK(expsmm)
   inds = (int*)calloc(L, sizeof(int));
   SSHT_ERROR_MEM_ALLOC_CHECK(inds)
 
@@ -73,27 +70,23 @@ void ssht_core_mw_forward_sov_conv_sym_ss_real(complex double *flm, double *f,
   exps_offset = L-1;
   for (m=0; m<=L-1; m++)
     expsm[m] = cexp(I*SSHT_PION2*(m+spin));
-/* for (mm=-(L-1); mm<=L-1; mm++) */
-/*   expsmm[mm + exps_offset] = cexp(-I*mm*SSHT_PI/(2.0*L-1.0)); */
 
   // Print messages depending on verbosity level.
   if (verbosity > 0) {
     printf("%s %s\n", SSHT_PROMPT, 
-	   "Computing forward transform using MW sampling with ");
+	   "Computing forward transform using MW symmetric sampling with ");
     printf("%s%s%d%s%d%s\n", SSHT_PROMPT, "parameters  (L,spin,reality) = (", 
 	   L, ",", spin, ", TRUE)");
     if (verbosity > 1)
       printf("%s %s\n", SSHT_PROMPT, 
-	     "Using routine ssht_core_mw_forward_sov_conv_sym_real...");
+	     "Using routine ssht_core_mw_forward_sov_conv_sym_ss_real...");
   }
 
   // Compute Fourier transform over phi, i.e. compute Fmt.
   // Note that t index (second index) is increased in size by one 
   // compared to usual sampling.
-//  Fmt = (complex double*)calloc(L*(2*L-1), sizeof(complex double));
   Fmt = (complex double*)calloc(L*(2*L), sizeof(complex double));
   SSHT_ERROR_MEM_ALLOC_CHECK(Fmt)
-//  Fmt_stride = 2*L-1;
   Fmt_stride = 2*L;
   Fmt_offset = L-1;
   f_stride = 2*L-1;
@@ -102,7 +95,6 @@ void ssht_core_mw_forward_sov_conv_sym_ss_real(complex double *flm, double *f,
   out = (complex double*)calloc(L, sizeof(complex double));
   SSHT_ERROR_MEM_ALLOC_CHECK(out)
   plan = fftw_plan_dft_r2c_1d(2*L-1, in_real, out, FFTW_MEASURE);
-//  for (t=0; t<=L-1; t++) {
   for (t=0; t<=L; t++) {
     memcpy(in_real, &f[t*f_stride], f_stride*sizeof(double));
         fftw_execute_dft_r2c(plan, in_real, out);
@@ -115,35 +107,27 @@ void ssht_core_mw_forward_sov_conv_sym_ss_real(complex double *flm, double *f,
 
   // Extend Fmt periodically.
   for (m=0; m<=L-1; m++) 
-//    for (t=L; t<=2*L-2; t++) 
     for (t=L+1; t<=2*L-1; t++) 
       Fmt[m*Fmt_stride + t] = 
-//	signs[abs(m)] * ssign * Fmt[m*Fmt_stride + (2*L-2-t)];
 	signs[abs(m)] * ssign * Fmt[m*Fmt_stride + (2*L-t)];
 
   // Compute Fourier transform over theta, i.e. compute Fmm.
   // Note that mm index (second index) is increased in size by one.
-//  Fmm = (complex double*)calloc(L*(2*L-1), sizeof(complex double));
   Fmm = (complex double*)calloc(L*(2*L), sizeof(complex double));
   SSHT_ERROR_MEM_ALLOC_CHECK(Fmm)
-//  Fmm_stride = 2*L-1;
   Fmm_stride = 2*L;
   Fmm_offset = L-1;
-//  inout = (complex double*)calloc(2*L-1, sizeof(complex double));
   inout = (complex double*)calloc(2*L, sizeof(complex double));
   SSHT_ERROR_MEM_ALLOC_CHECK(inout)
-//  plan = fftw_plan_dft_1d(2*L-1, inout, inout, FFTW_FORWARD, FFTW_MEASURE);
   plan = fftw_plan_dft_1d(2*L, inout, inout, FFTW_FORWARD, FFTW_MEASURE);
   for (m=0; m<=L-1; m++) {
     memcpy(inout, &Fmt[m*Fmt_stride], Fmt_stride*sizeof(complex double));
     fftw_execute_dft(plan, inout, inout);
     for(mm=0; mm<=L-1; mm++) 
       Fmm[m*Fmm_stride + mm + Fmm_offset] = 
-//	inout[mm] / (2.0*L-1.0);
 	inout[mm] / (2.0*L);
     for(mm=-(L-1); mm<=-1; mm++) 
       Fmm[m*Fmm_stride + mm + Fmm_offset] = 
-//	inout[mm+2*L-1] / (2.0*L-1.0);
 	inout[mm+2*L-1+1] / (2.0*L);
   }
   fftw_destroy_plan(plan);
@@ -323,7 +307,6 @@ void ssht_core_mw_forward_sov_conv_sym_ss_real(complex double *flm, double *f,
   free(sqrt_tbl);
   free(signs); 
   free(expsm);
-//  free(expsmm);
   free(inds);
 
   // Print finished if verbosity set.
@@ -381,18 +364,17 @@ void ssht_core_mw_inverse_sov_sym_ss_real(double *f, complex double *flm,
   // Print messages depending on verbosity level.
   if (verbosity > 0) {
     printf("%s %s\n", SSHT_PROMPT, 
-	   "Computing inverse transform using MW sampling with ");
+	   "Computing inverse transform using MW symmetric sampling with ");
     printf("%s%s%d%s%d%s\n", SSHT_PROMPT, "parameters  (L,spin,reality) = (", 
 	   L, ",", spin, ", TRUE)");
     if (verbosity > 1)
       printf("%s %s\n", SSHT_PROMPT, 
-	     "Using routine ssht_core_mw_inverse_sov_sym_real...");
+	     "Using routine ssht_core_mw_inverse_sov_sym_ss_real...");
   }
 
   // Compute Fmm.
   // Note that mm index (first index) is increased in size by one and
   // will be filled with zeros by calloc.
-//  Fmm = (complex double*)calloc((2*L-1)*L, sizeof(complex double));
   Fmm = (complex double*)calloc((2*L)*L, sizeof(complex double));
   SSHT_ERROR_MEM_ALLOC_CHECK(Fmm)
   Fmm_offset = L-1;
@@ -457,26 +439,9 @@ void ssht_core_mw_inverse_sov_sym_ss_real(double *f, complex double *flm,
 	signs[abs(m)] * ssign 
 	* Fmm[(-mm + Fmm_offset)*Fmm_stride + m];
 
-  /* // Apply phase modulation to account for sampling offset. */
-  /* for (mm=-(L-1); mm<=L-1; mm++) { */
-  /*   mmfactor = cexp(I*mm*SSHT_PI/(2.0*L-1.0)); */
-  /*   for (m=0; m<=L-1; m++)  */
-  /*     Fmm[(mm + Fmm_offset)*Fmm_stride + m] *=  */
-  /* 	mmfactor; */
-  /* } */
-
   // Apply spatial shift.
-//  Fmm_shift = (complex double*)calloc((2*L-1)*L, sizeof(complex double));
   Fmm_shift = (complex double*)calloc((2*L)*L, sizeof(complex double));
   SSHT_ERROR_MEM_ALLOC_CHECK(Fmm_shift)
-  /* for (mm=0; mm<=L-1; mm++) */
-  /*   for (m=0; m<=L-1; m++) */
-  /*     Fmm_shift[mm*Fmm_stride + m] =  */
-  /* 	Fmm[(mm + Fmm_offset)*Fmm_stride + m]; */
-  /* for (mm=-(L-1); mm<=-1; mm++) */
-  /*   for (m=0; m<=L-1; m++) */
-  /*     Fmm_shift[(mm + 2*L-1)*Fmm_stride + m] =  */
-  /* 	Fmm[(mm + Fmm_offset)*Fmm_stride + m]; */
   for (mm=0; mm<=L-1; mm++)
     for (m=0; m<=L-1; m++)
       Fmm_shift[mm*Fmm_stride + m] = 
@@ -487,14 +452,11 @@ void ssht_core_mw_inverse_sov_sym_ss_real(double *f, complex double *flm,
 	Fmm[(mm + Fmm_offset)*Fmm_stride + m];
 
   // Allocate space for function values.
-//  fext_real = (double*)calloc((2*L-1)*(2*L-1), sizeof(double));
   fext_real = (double*)calloc((2*L)*(2*L-1), sizeof(double));
   SSHT_ERROR_MEM_ALLOC_CHECK(fext_real)
   fext_stride = 2*L-1;
 
   // Perform 2D FFT.  
-//  plan = fftw_plan_dft_c2r_2d(2*L-1, 2*L-1, Fmm_shift, fext_real, 
-//			      FFTW_ESTIMATE);
   plan = fftw_plan_dft_c2r_2d(2*L, 2*L-1, Fmm_shift, fext_real, 
 			      FFTW_ESTIMATE);
   fftw_execute_dft_c2r(plan, Fmm_shift, fext_real);
@@ -505,14 +467,7 @@ void ssht_core_mw_inverse_sov_sym_ss_real(double *f, complex double *flm,
   free(Fmm_shift);
 
   // Extract f from version of f extended to the torus (fext).
-//  memcpy(f, fext_real, L*(2*L-1)*sizeof(double));
   memcpy(f, fext_real, (L+1)*(2*L-1)*sizeof(double));
-
-  /* Memcpy equivalent to:
-  for (t=0; t<=L-1; t++)
-    for (p=0; p<=2*L-2; p++)
-      f[t*fext_stride + p] = fext[t*fext_stride + p];
-  */
 
   // Free fext memory.
   free(fext_real);
@@ -662,7 +617,7 @@ void ssht_core_mw_inverse_sov_sym_ss(complex double *f, complex double *flm,
   // Print messages depending on verbosity level.
   if (verbosity > 0) {
     printf("%s %s\n", SSHT_PROMPT, 
-	   "Computing inverse transform using MW sampling with ");
+	   "Computing inverse transform using MW symmetric sampling with ");
     printf("%s%s%d%s%d%s\n", SSHT_PROMPT, "parameters  (L,spin,reality) = (", 
 	   L, ",", spin, ", FALSE)");
     if (verbosity > 1)
@@ -856,12 +811,12 @@ void ssht_core_mw_forward_sov_conv_sym_ss(complex double *flm, complex double *f
   // Print messages depending on verbosity level.
   if (verbosity > 0) {
     printf("%s %s\n", SSHT_PROMPT, 
-	   "Computing forward transform using MW sampling with ");
+	   "Computing forward transform using MW symmetric sampling with ");
     printf("%s%s%d%s%d%s\n", SSHT_PROMPT, "parameters  (L,spin,reality) = (", 
 	   L, ",", spin, ", FALSE)");
     if (verbosity > 1)
       printf("%s %s\n", SSHT_PROMPT, 
-	     "Using routine ssht_core_mw_forward_sov_conv_sym...");
+	     "Using routine ssht_core_mw_forward_sov_conv_sym_ss...");
   }
 
   // Compute Fourier transform over phi, i.e. compute Fmt.
