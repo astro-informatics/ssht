d57a2c1979d553d6809e7750e0577aeb15c018ba
Jason McEwen <jason.mcewen@ucl.ac.uk>
2011-03-31 17:22:08 +0000

    Added matlab function to plot spherical harmonic coefficients.

---
+++ src/matlab/ssht_plot_harmonic.m
@@ -0,0 +1,28 @@
+function ssht_plot_harmonic(flm, L)
+% ssht_plot_harmonic - Plot spherical harmonic coefficients
+%
+% Plots spherical harmonic coefficients.
+%
+% Default usage is given by
+%
+%   ssht_plot_harmonic(f, L)
+%
+% where f is the sampled function and L is the harmonic band-limit.
+%
+% Author: Jason McEwen (www.jasonmcewen.org)
+
+% SSHT package to perform spin spherical harmonic transforms
+% Copyright (C) 2011  Jason McEwen
+% See LICENSE.txt for license details
+
+flm_plot = zeros(2*L-1, L);
+
+% Impose reality on flms.
+for el = 0:L-1   
+   for m = -el:el
+      ind = ssht_elm2ind(el, m);
+      flm_plot(m+L+1, el+1) = flm(ind);                  
+   end
+end
+
+imagesc(flm_plot);
