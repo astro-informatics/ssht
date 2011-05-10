7d72f75a62aeb49d4b75d2047ab89177c08ba1f5
Jason McEwen <jason.mcewen@ucl.ac.uk>
2011-05-10 13:15:36 +0000

    Tidied and optimisied adjoint forward algorithm for MWSS sampling for real signals.

--- src/c/ssht_adjoint.c
+++ src/c/ssht_adjoint.c
@@ -1083,7 +1083,6 @@ void ssht_adjoint_mw_forward_sov_sym_real(double *f,
 	SSHT_ERROR_GENERIC("Invalid dl method") 
     }
 
-
     // Compute Fmm.
     elfactor = sqrt((double)(2.0*el+1.0)/(4.0*SSHT_PI));
     el2pel = el *el + el;    
@@ -2185,19 +2184,17 @@ void ssht_adjoint_mw_forward_sov_sym_ss(complex double *f, complex double *flm,
  * \author <a href="http://www.jasonmcewen.org">Jason McEwen</a>
  */
 void ssht_adjoint_mw_forward_sov_sym_ss_real(double *f, 
-					  complex double *flm,
-					  int L, int spin,
-					  ssht_dl_method_t dl_method,
-					  int verbosity) {
+					     complex double *flm,
+					     int L, int spin,
+					     ssht_dl_method_t dl_method,
+					     int verbosity) {
 
   int el, m, mm, ind;
-  //int t, p;
   int eltmp;
   double *sqrt_tbl, *signs;
   int el2pel, inds_offset;
   int *inds;
   double ssign, elfactor;
-  complex double mmfactor;
   double *dl;
   double *dl8 = NULL;
   int dl_offset, dl_stride;
@@ -2205,30 +2202,20 @@ void ssht_adjoint_mw_forward_sov_sym_ss_real(double *f,
   int exps_offset;
   double elmmsign, elssign;
   int spinneg;
-  complex double *Fmm, *fext;
-  int Fmm_offset, Fmm_stride, fext_stride;
+  complex double *Fmm;
+  int Fmm_offset, Fmm_stride;
 
   fftw_plan plan, plan_bwd, plan_fwd;
   complex double *Ftm, *Gmm;
   complex double *w, *wr;
   int w_offset;
   complex double *Fmm_pad, *tmp_pad;
-  int f_stride, Ftm_stride, Ftm_offset;
+  int f_stride, Ftm_stride;
   int r, t, p;
   complex double *inout;
-
   complex double *in;
   double *out_real;
-
-
-
-
-  int Gmm_full_offset, Gmm_full_stride;
-  complex double *Gmm_full;
-
-
   int Gmm_offset, Gmm_stride;
-
 
   // Allocate memory.
   sqrt_tbl = (double*)calloc(2*(L-1)+2, sizeof(double));
@@ -2256,23 +2243,19 @@ void ssht_adjoint_mw_forward_sov_sym_ss_real(double *f,
   // Print messages depending on verbosity level.
   if (verbosity > 0) {
     printf("%s %s\n", SSHT_PROMPT,
-	   "Computing inverse transform using MW sampling with ");
+	   "Computing adjoint forward transform using MW symmetric sampling with ");
     printf("%s%s%d%s%d%s\n", SSHT_PROMPT, "parameters  (L,spin,reality) = (",
 	   L, ",", spin, ", FALSE)");
     if (verbosity > 1)
       printf("%s %s\n", SSHT_PROMPT,
-	     "Using routine ssht_core_mw_inverse_sov_sym...");
+	     "Using routine ssht_adjoint_mw_forward_sov_sym_ss_real...");
   }
 
-
-  // ==========================
-  // 1.
-
- // Compute Fmm.
-  Fmm = (complex double*)calloc((2*L-1)*L, sizeof(complex double));
+  // Compute Fmm.
+  Fmm = (complex double*)calloc(L*(2*L-1), sizeof(complex double));
   SSHT_ERROR_MEM_ALLOC_CHECK(Fmm)
   Fmm_offset = L-1;
-  Fmm_stride = L;    
+  Fmm_stride = 2*L-1;    
   dl = ssht_dl_calloc(L, SSHT_DL_QUARTER);
   SSHT_ERROR_MEM_ALLOC_CHECK(dl)
   if (dl_method == SSHT_DL_RISBO) {
@@ -2337,7 +2320,6 @@ void ssht_adjoint_mw_forward_sov_sym_ss_real(double *f,
 	SSHT_ERROR_GENERIC("Invalid dl method") 
     }
 
-
     // Compute Fmm.
     elfactor = sqrt((double)(2.0*el+1.0)/(4.0*SSHT_PI));
     el2pel = el *el + el;    
@@ -2349,7 +2331,7 @@ void ssht_adjoint_mw_forward_sov_sym_ss_real(double *f,
 
       for (m=0; m<=el; m++) {
 	ind = inds[m + inds_offset];
-    	Fmm[(mm + Fmm_offset)*Fmm_stride + m] +=
+    	Fmm[m*Fmm_stride + mm + Fmm_offset] +=
     	  ssign
     	  * elfactor
 	  * exps[m + exps_offset]    	  
@@ -2368,29 +2350,11 @@ void ssht_adjoint_mw_forward_sov_sym_ss_real(double *f,
     free(dl8);
 
   // Use symmetry to compute Fmm for negative mm.
-  for (mm=-(L-1); mm<=-1; mm++) 
-    for (m=0; m<=L-1; m++) 
-      Fmm[(mm + Fmm_offset)*Fmm_stride + m] = 
+  for (m=0; m<=L-1; m++) 
+    for (mm=-(L-1); mm<=-1; mm++) 
+      Fmm[m*Fmm_stride + mm + Fmm_offset] =      
 	signs[abs(m)] * ssign 
-	* Fmm[(-mm + Fmm_offset)*Fmm_stride + m];
-
-
-
-
-  /* // Apply phase modulation to account for sampling offset. */
-  /* for (mm=-(L-1); mm<=L-1; mm++) { */
-  /*   mmfactor = cexp(I*mm*SSHT_PI/(2.0*L-1.0)); */
-  /*   for (m=-(L-1); m<=L-1; m++) */
-  /*     Fmm[(mm + Fmm_offset)*Fmm_stride + m + Fmm_offset] *= */
-  /* 	mmfactor; */
-  /* } */
-
-
-
-
-  // ==========================
-  // 2.
-
+	* Fmm[m*Fmm_stride - mm + Fmm_offset];
 
   // Compute weights.
   w = (double complex*)calloc(4*L-3, sizeof(complex double));
@@ -2421,11 +2385,9 @@ void ssht_adjoint_mw_forward_sov_sym_ss_real(double *f,
   SSHT_ERROR_MEM_ALLOC_CHECK(Fmm_pad)
   tmp_pad = (complex double*)calloc(4*L-3, sizeof(complex double));
   SSHT_ERROR_MEM_ALLOC_CHECK(tmp_pad)
-  Gmm = (complex double*)calloc((2*L)*(L+1), sizeof(complex double));
-  Gmm_stride = L+1;
+  Gmm = (complex double*)calloc((L+1)*(2*L), sizeof(complex double));
+  Gmm_stride = 2*L;
   Gmm_offset = L-1;
-//  Gmm = (complex double*)calloc((2*L-1)*L, sizeof(complex double));
-  //Gmm_stride = L;
   SSHT_ERROR_MEM_ALLOC_CHECK(Gmm)
   for (m=0; m<=L-1; m++) {
 
@@ -2436,8 +2398,7 @@ void ssht_adjoint_mw_forward_sov_sym_ss_real(double *f,
       Fmm_pad[mm+w_offset] = 0.0;
     for (mm=-(L-1); mm<=L-1; mm++)
       Fmm_pad[mm + w_offset] = 
-	Fmm[(mm+Fmm_offset)*Fmm_stride + m];
-//	Fmm[m*Fmm_stride + mm + Fmm_offset];
+	Fmm[m*Fmm_stride + mm + Fmm_offset];
 
     // Compute IFFT of Fmm.
     for (mm=1; mm<=2*L-2; mm++)
@@ -2467,44 +2428,27 @@ void ssht_adjoint_mw_forward_sov_sym_ss_real(double *f,
 
     // Extract section of Gmm of interest.
     for (mm=-(L-1); mm<=L-1; mm++)
-      Gmm[(mm+Gmm_offset)*Gmm_stride + m] = 
+      Gmm[m*Gmm_stride + mm + Gmm_offset] = 
 	Fmm_pad[mm + w_offset] * 2.0 * SSHT_PI / (4.0*L-3.0);
 
   }
   fftw_destroy_plan(plan_bwd);
   fftw_destroy_plan(plan_fwd);
-
-
-
-
-
-
-  // ==========================
-  // 3. Gmm -> Fmt
-
-
-//============================================================================
-
-
-
+  free(inout);
+  
+  // Compute Fourier transform over theta.
   inout = (complex double*)calloc(2*L, sizeof(complex double));
   SSHT_ERROR_MEM_ALLOC_CHECK(inout)
   plan = fftw_plan_dft_1d(2*L, inout, inout, FFTW_BACKWARD, FFTW_MEASURE);
-
-
-
-
-  // Compute Fourier transform over theta.
   Ftm = (complex double*)calloc((2*L)*(L+1), sizeof(complex double));
   SSHT_ERROR_MEM_ALLOC_CHECK(Ftm)
   Ftm_stride = L+1;
-  //Ftm_offset = L-1;
   for (m=0; m<=L; m++) {
 
     for(mm=0; mm<=L; mm++)
-      inout[mm] = Gmm[(mm+Gmm_offset)*Gmm_stride + m];
+      inout[mm] = Gmm[m*Gmm_stride + mm + Gmm_offset];
     for(mm=-(L-1); mm<=-1; mm++)
-      inout[mm+2*L-1+1] = Gmm[(mm+Gmm_offset)*Gmm_stride + m];
+      inout[mm+2*L-1+1] = Gmm[m*Gmm_stride + mm + Gmm_offset];
     fftw_execute_dft(plan, inout, inout);
 
     for(t=0; t<=2*L-1; t++)
@@ -2514,11 +2458,6 @@ void ssht_adjoint_mw_forward_sov_sym_ss_real(double *f,
   fftw_destroy_plan(plan);
   free(inout);
 
-
-
-  // ==========================
-  // 4.
-
   // Adjoint of periodic extension of Ftm.
   for(t=1; t<=L-1; t++)
     for (m=0; m<=L; m++)
@@ -2526,20 +2465,12 @@ void ssht_adjoint_mw_forward_sov_sym_ss_real(double *f,
   	Ftm[t*Ftm_stride + m]
   	+ signs[abs(m)] * ssign * Ftm[(2*L-t)*Ftm_stride + m];
 
-  // ==========================
-  // 5.
-
-
+  // Compute Fourier transform over phi.
   out_real = (double*)calloc(2*L, sizeof(double));
   SSHT_ERROR_MEM_ALLOC_CHECK(out_real)
   in = (complex double*)calloc(L+1, sizeof(complex double));
   SSHT_ERROR_MEM_ALLOC_CHECK(in)
   plan = fftw_plan_dft_c2r_1d(2*L, in, out_real, FFTW_MEASURE);
-
-
-
-
-  // Compute Fourier transform over phi.
   f_stride = 2*L;
   for(t=0; t<=L; t++) {
     
@@ -2549,29 +2480,29 @@ void ssht_adjoint_mw_forward_sov_sym_ss_real(double *f,
     for(p=0; p<=2*L-1; p++)
       f[t*f_stride + p] = out_real[p] / (2.0*L);
 
-
   }
   fftw_destroy_plan(plan);
-
   free(out_real);
   free(in);
 
-
-
-
-  // Free Fmm memory.
+  // Free memory.
   free(Fmm);
-  
-
-  // Print finished if verbosity set.
-  if (verbosity > 0)
-    printf("%s %s", SSHT_PROMPT, "Inverse transform computed!");
+  free(Ftm);
+  free(w);
+  free(wr);
+  free(Fmm_pad);
+  free(tmp_pad);
+  free(Gmm);
 
   // Free precomputation memory.
   free(sqrt_tbl);
   free(signs);
   free(exps);
   free(inds);
+
+  // Print finished if verbosity set.
+  if (verbosity > 0)
+    printf("%s %s", SSHT_PROMPT, "Adjoint forward transform computed!");
 
 }
 
