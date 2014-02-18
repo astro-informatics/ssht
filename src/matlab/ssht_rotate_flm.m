333eca57ebe1df63158a1f58d1703e349cc7b34c
Martin Büttner <m.buettner.d@gmail.com>
2014-02-18 15:31:57 +0000

    Implement matlab function to rotate functions on sphere
    ssht_rotate_flm can now rotate any function on the sphere in harmonic
    space. There is also ssht_demo_rotate which showcases this on a
    spherical harmonic.

--- src/matlab/ssht_rotate_flm.m
+++ src/matlab/ssht_rotate_flm.m
@@ -1,4 +1,4 @@
-function [flm_rotated] = ssht_rotate_flm(flm, dlm, alpha, gamma, varargin)
+function [flm_rotated] = ssht_rotate_flm(flm, d, alpha, gamma, varargin)
 % ssht_rotate_flm - Rotate a function in harmonic space
 %
 % Rotates a function on the sphere in harmonic space, where
@@ -7,11 +7,13 @@ function [flm_rotated] = ssht_rotate_flm(flm, dlm, alpha, gamma, varargin)
 %
 % Default usage is given by
 %
-%   [flm_rotated] = ssht_rotate_flm(flm, dlm, alpha, gamma, <option>)
+%   [flm_rotated] = ssht_rotate_flm(flm, d, alpha, gamma, <option>)
 %
 % where flm are the harmonic coefficients of the function to be
-% rotated, dlm is the Wigner small-d functions d_lm evaluated
-% at beta and alpha and gamma are the other two rotation angles.
+% rotated, d are the Wigner small-d functions d_lmn for all el,
+% m, n evaluated at beta. They are indexed d(el,m,n), such that
+% d has dimensions (L,2*L-1,2*L-1).
+% alpha and gamma are the other two rotation angles.
 % flm_rotated are the harmonic coefficients of the resulting
 % function after rotation.
 % The band-limit L is implied by the size of the flm, which
@@ -29,3 +31,40 @@ function [flm_rotated] = ssht_rotate_flm(flm, dlm, alpha, gamma, varargin)
 % SSHT package to perform spin spherical harmonic transforms
 % Copyright (C) 2011-2014 Jason McEwen
 % See LICENSE.txt for license details
+
+
+% Parse arguments.
+p = inputParser;
+p.addRequired('flm', @isnumeric);
+p.addRequired('d', @isnumeric);
+p.addRequired('alpha', @isnumeric);
+p.addRequired('gamma', @isnumeric);
+p.addParamValue('M', -1, @isnumeric);
+
+p.parse(flm, d, alpha, gamma, varargin{:});
+
+args = p.Results;
+
+L = sqrt(length(flm));
+
+if args.M == -1
+    args.M = L;
+end
+
+M = args.M;
+
+index = 1;
+
+flm_rotated = zeros(L^2,1);
+
+for el = 0:L-1
+    for m = -el:el
+        n_max = min(el, M-1);
+        for n = -n_max:n_max
+            Dlmn = exp(-i*m*alpha) * d(el+1,m+L,n+L) * exp(-i*n*gamma);
+            flm_rotated(index) = flm_rotated(index) + ...
+                Dlmn * flm(ssht_elm2ind(el,n));
+        end
+        index = index + 1;
+    end
+end
