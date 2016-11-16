d8c7c564e4fc1bd9095e65b9579fcbe5c8c54c8e
luke <Luke.Pratley.15@ucl.ac.uk>
2016-11-16 17:11:36 +0000

    added macro for cpp and c versions of the complex data type

--- include/c/ssht.h
+++ include/c/ssht.h
@@ -2,6 +2,24 @@
 #ifndef SSHT
 #define SSHT
 
+
+#ifdef __cplusplus
+#define SSHT_COMPLEX(TYPE) std::complex<TYPE>
+extern "C" {
+#else
+#include <complex>
+#define SSHT_COMPLEX(TYPE) TYPE complex
+#endif
+
+
+#ifdef __cplusplus
+}
+#endif
+
+SSHT_COMPLEX(double) twice(SSHT_COMPLEX(double) input) {
+  return 2. * input;
+}
+
 #include "../../src/c/ssht_types.h"
 #include "../../src/c/ssht_error.h"
 #include "../../src/c/ssht_sampling.h"
@@ -9,4 +27,5 @@
 #include "../../src/c/ssht_core.h"
 #include "../../src/c/ssht_adjoint.h"
 
+#undef SSHT_COMPLEX
 #endif
