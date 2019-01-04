f0e51681dcec08f8dac8d9f92099d8f01f006c31
paddyroddy <patrickjamesroddy@gmail.com>
2019-01-04 19:11:26 +0000

    port all tests to python3 and reformat using pep8

--- src/python/pyssht_demo_5.py
+++ src/python/pyssht_demo_5.py
@@ -8,7 +8,7 @@ import os
 # % pyssht_demo_5 - Run demo5
 # %
 # % Smooth Earth topography map by applying a Gaussian filter in harmonic
-# % space.  
+# % space.
 # %
 # % The official Earth Gravitational Model EGM2008 has been publicly
 # % released by the U.S. National Geospatial-Intelligence Agency (NGA) (these
@@ -21,40 +21,44 @@ import os
 
 # Define parameters.
 L = 128
-sigma = np.pi/L;
+sigma = np.pi / L
 
-#% Load harmonic coefficients of Earth.
-matfile = os.path.join(os.path.dirname(__file__),os.pardir,'matlab','data','EGM2008_Topography_flms_L0128')
+# % Load harmonic coefficients of Earth.
+matfile = os.path.join(os.path.dirname(__file__), os.pardir,
+                       'matlab', 'data', 'EGM2008_Topography_flms_L0128')
 mat_contents = sio.loadmat(matfile)
 
-flm     = np.ascontiguousarray(mat_contents['flm'][:,0])
+flm = np.ascontiguousarray(mat_contents['flm'][:, 0])
 
-#% Smooth harmonic coefficients.
+# % Smooth harmonic coefficients.
 flm_smooth = ssht.guassian_smoothing(flm, L, sigma)
 
 # Compute real space version of Earth.
 f = ssht.inverse(flm, L, Reality=True)
 f_smooth = ssht.inverse(flm_smooth, L, Reality=True)
 
-
-# Plot 
-f_plot, mask_array = ssht.mollweide_projection(f, L, resolution=200, rot=[0.0,np.pi,np.pi])
+# Plot
+f_plot, mask_array = ssht.mollweide_projection(
+    f, L, resolution=200, rot=[0.0, np.pi, np.pi])
 plt.figure()
-plt.subplot(1,2,1)
-imgplot = plt.imshow(f_plot,interpolation='nearest')
-plt.colorbar(imgplot,fraction=0.025, pad=0.04)
-plt.imshow(mask_array, interpolation='nearest', cmap=cm.gray, vmin=-1., vmax=1.)
-plt.gca().set_aspect("equal")
-plt.title("f")
+plt.subplot(1, 2, 1)
+imgplot = plt.imshow(f_plot, interpolation='nearest')
+plt.colorbar(imgplot, fraction=0.025, pad=0.04)
+plt.imshow(mask_array, interpolation='nearest',
+           cmap=cm.gray, vmin=-1., vmax=1.)
+plt.gca().set_aspect('equal')
+plt.title('f')
 plt.axis('off')
 
-f_plot, mask_array = ssht.mollweide_projection(f_smooth, L, resolution=200, rot=[0.0,np.pi,np.pi])
-plt.subplot(1,2,2)
-imgplot = plt.imshow(f_plot,interpolation='nearest')
-plt.colorbar(imgplot,fraction=0.025, pad=0.04)
-plt.imshow(mask_array, interpolation='nearest', cmap=cm.gray, vmin=-1., vmax=1.)
-plt.gca().set_aspect("equal")
-plt.title("f smooth")
+f_plot, mask_array = ssht.mollweide_projection(
+    f_smooth, L, resolution=200, rot=[0.0, np.pi, np.pi])
+plt.subplot(1, 2, 2)
+imgplot = plt.imshow(f_plot, interpolation='nearest')
+plt.colorbar(imgplot, fraction=0.025, pad=0.04)
+plt.imshow(mask_array, interpolation='nearest',
+           cmap=cm.gray, vmin=-1., vmax=1.)
+plt.gca().set_aspect('equal')
+plt.title('f smooth')
 plt.axis('off')
 
 plt.show()
--- src/python/pyssht_demo_7.py
+++ src/python/pyssht_demo_7.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 from pylab import cm
 import scipy.io as sio
 
-
 # % pyssht_demo_7 - Run demo7
 # %
 # % Plot spherical harmonic function on the sphere, then
@@ -15,48 +14,49 @@ import scipy.io as sio
 # % pyssht python package to perform spin spherical harmonic transforms
 
 # Define parameters.
-L = 64;
+L = 64
 el = 4
 m = 2
-gamma = np.pi/2
-beta = np.pi/4
-alpha = -np.pi/2
+gamma = np.pi / 2
+beta = np.pi / 4
+alpha = -np.pi / 2
 
 # Generate spherical harmonics.
-flm = np.zeros((L*L),dtype=complex)
+flm = np.zeros((L * L), dtype=complex)
 ind = ssht.elm2ind(el, m)
 flm[ind] = 1.0
 
 # Compute function on the sphere.
 f = ssht.inverse(flm, L)
 
-
 # Rotate spherical harmonic
 flm_rot = ssht.rotate_flms(flm, alpha, beta, gamma, L)
-
 
 # Compute rotated function on the sphere.
 f_rot = ssht.inverse(flm_rot, L)
 
-
 # Plot
-f_plot, mask_array, f_plot_imag, mask_array_imag = ssht.mollweide_projection(f, L, resolution=200, rot=[0.0,np.pi,np.pi])
+f_plot, mask_array, f_plot_imag, mask_array_imag = ssht.mollweide_projection(
+    f, L, resolution=200, rot=[0.0, np.pi, np.pi])
 plt.figure()
-plt.subplot(1,2,1)
-imgplot = plt.imshow(f_plot,interpolation='nearest')
-plt.colorbar(imgplot,fraction=0.025, pad=0.04)
-plt.imshow(mask_array, interpolation='nearest', cmap=cm.gray, vmin=-1., vmax=1.)
-plt.gca().set_aspect("equal")
-plt.title("f")
+plt.subplot(1, 2, 1)
+imgplot = plt.imshow(f_plot, interpolation='nearest')
+plt.colorbar(imgplot, fraction=0.025, pad=0.04)
+plt.imshow(mask_array, interpolation='nearest',
+           cmap=cm.gray, vmin=-1., vmax=1.)
+plt.gca().set_aspect('equal')
+plt.title('f')
 plt.axis('off')
 
-f_plot, mask_array, f_plot_imag, mask_array_imag = ssht.mollweide_projection(f_rot, L, resolution=200, rot=[0.0,np.pi,np.pi])
-plt.subplot(1,2,2)
-imgplot = plt.imshow(f_plot,interpolation='nearest')
-plt.colorbar(imgplot,fraction=0.025, pad=0.04)
-plt.imshow(mask_array, interpolation='nearest', cmap=cm.gray, vmin=-1., vmax=1.)
-plt.gca().set_aspect("equal")
-plt.title("f rot")
+f_plot, mask_array, f_plot_imag, mask_array_imag = ssht.mollweide_projection(
+    f_rot, L, resolution=200, rot=[0.0, np.pi, np.pi])
+plt.subplot(1, 2, 2)
+imgplot = plt.imshow(f_plot, interpolation='nearest')
+plt.colorbar(imgplot, fraction=0.025, pad=0.04)
+plt.imshow(mask_array, interpolation='nearest',
+           cmap=cm.gray, vmin=-1., vmax=1.)
+plt.gca().set_aspect('equal')
+plt.title('f rot')
 plt.axis('off')
 
 plt.show()
