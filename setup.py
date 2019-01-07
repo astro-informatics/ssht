9d62402aa8d3bf78c17c97930abe721808524674
paddyroddy <patrickjamesroddy@gmail.com>
2019-01-07 12:30:19 +0000

    reformat pep8

--- setup.py
+++ setup.py
@@ -9,35 +9,33 @@ from Cython.Build import cythonize
 import numpy
 
 # clean previous build
-for root, dirs, files in os.walk("./src/python/", topdown=False):
+for root, dirs, files in os.walk('./src/python/', topdown=False):
     for name in dirs:
-        if (name == "build"):
+        if (name == 'build'):
             shutil.rmtree(name)
-
 
 include_dirs = [
     numpy.get_include(),
-    "./include",
-    "./include/c",
-    ]
+    './include',
+    './include/c',
+]
 
-extra_link_args=[
-    "-L./lib/c",
-    "-L"+os.environ['FFTW']+"/lib",
+extra_link_args = [
+    '-L./lib/c',
+    '-L' + os.environ['FFTW'] + '/lib',
 ]
 
 setup(
-    classifiers=['Programming Language :: Python :: 2.7'],
-    name = "pyssht",
-    version = "2.0",
+    name='pyssht',
+    version='2.0',
     prefix='.',
     cmdclass={'build_ext': build_ext},
     ext_modules=cythonize([Extension(
-        "src/python/pyssht",
+        'src/python/pyssht',
         package_dir=['src'],
-        sources=["src/python/pyssht.pyx"],
+        sources=['src/python/pyssht.pyx'],
         include_dirs=include_dirs,
-        libraries=["ssht", "fftw3"],
+        libraries=['ssht', 'fftw3'],
         extra_link_args=extra_link_args,
         extra_compile_args=[]
     )])
