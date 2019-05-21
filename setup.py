7286476b41563ea61f7530b9c9966403184e3415
Mayeul d'Avezac <2745737+mdavezac@users.noreply.github.com>
2019-05-21 22:52:46 +0100

    Missing docstring file

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
+from sys import platform
+
 from skbuild import setup
+
+cmake_args = ["-Dpython:BOOL=ON", "-Dopenmp:BOOL=OFF", "-Dtests:BOOL=OFF"]
+if platform.lower() == "darwin":
+    cmake_args.append("-DCMAKE_OSX_DEPLOYMENT_TARGET:STRING=10.9")
 
 setup(
     name="pyssht",
@@ -11,12 +17,8 @@ setup(
     description="Fast spin spherical transforms",
     url="http://astro-informatics.github.io/ssht/",
     package_dir={"pyssht": "src/pyssht"},
-    cmake_args=[
-        "-Dpython:BOOL=ON",
-        "-Dopenmp:BOOL=OFF",
-        "-Dtests:BOOL=OFF",
-        "-DCMAKE_OSX_DEPLOYMENT_TARGET:STRING=10.9",
-    ],
+    package_data={"pyssht": ["SSHT_Python_Documentation.md"]},
+    cmake_args=cmake_args,
     cmake_languages=("C",),
     license="GPL-2",
     packages=["pyssht"],
