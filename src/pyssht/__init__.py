e5e9502f9e090e1a43724b68e1d9e43d52a3da7e
Mayeul d'Avezac <2745737+mdavezac@users.noreply.github.com>
2019-09-10 08:15:02 +0100

    Incorrect capitalization
    Closes #11

--- src/pyssht/__init__.py
+++ src/pyssht/__init__.py
@@ -1,6 +1,6 @@
 """SSHT module."""
 
 from pathlib import Path
-__doc__ = (Path(__file__).parent / "SSHT_Python_documentation.md").read_text()
+__doc__ = (Path(__file__).parent / "SSHT_Python_Documentation.md").read_text()
 
 from .pyssht import *
