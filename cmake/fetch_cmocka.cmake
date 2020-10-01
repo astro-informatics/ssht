e06761668d6cfaebc44c605b46dd1c45b235f8a3
Mayeul d'Avezac <2745737+mdavezac@users.noreply.github.com>
2020-10-01 23:33:38 +0100

    Simplify patch for cmocka

--- cmake/fetch_cmocka.cmake
+++ cmake/fetch_cmocka.cmake
@@ -8,13 +8,15 @@ FetchContent_Declare(
 if(NOT cmocka_POPULATED)
   # Fetch the content using previously declared details
   FetchContent_Populate("cmocka")
-  file(READ "${cmocka_SOURCE_DIR}/CMakeLists.txt" patch_cmocka )
-  string(REPLACE "add_subdirectory(doc)" "" patch_cmocka ${patch_cmocka})
-  string(REPLACE "add_subdirectory(tests)" "" patch_cmocka ${patch_cmocka})
-  string(REPLACE "add_subdirectory(example)" "" patch_cmocka ${patch_cmocka})
-  file(WRITE "${cmocka_SOURCE_DIR}/CMakeLists.txt" ${patch_cmocka} )
+  file(READ "${cmocka_SOURCE_DIR}/CMakeLists.txt" patch_cmocka)
+  string(REPLACE "add_subdirectory(src)" "add_subdirectory(src)\nreturn()"
+                 patch_cmocka ${patch_cmocka})
+  file(WRITE "${cmocka_SOURCE_DIR}/CMakeLists.txt" ${patch_cmocka})
 
   add_subdirectory(${cmocka_SOURCE_DIR} ${cmocka_BINARY_DIR})
 endif()
 
-find_file(CSTDLIB_HEADERS NAMES setjmp.h PATHS /usr/include)
+find_file(
+  CSTDLIB_HEADERS
+  NAMES setjmp.h
+  PATHS /usr/include)
