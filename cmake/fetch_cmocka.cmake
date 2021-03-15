77c42ed8481d1737ea0e6aa7cc9a1b52e1bdbbd8
Mayeul d'Avezac <2745737+mdavezac@users.noreply.github.com>
2021-03-15 14:48:30 +0000

    Compile against static cmocka because simpler

--- cmake/fetch_cmocka.cmake
+++ cmake/fetch_cmocka.cmake
@@ -5,6 +5,9 @@ FetchContent_Declare(
   GIT_REPOSITORY https://gitlab.com/cmocka/cmocka.git
   GIT_TAG cmocka-1.1.5)
 
+set(WITH_STATIC_LIB ON CACHE BOOL "CMocka: Build with a static library" FORCE)
+set(WITH_CMOCKERY_SUPPORT OFF CACHE BOOL "CMocka: Install a cmockery header" FORCE)
+set(PICKY_DEVELOPER OFF CACHE BOOL "CMocka: Build with picky developer flags" FORCE)
 FetchContent_GetProperties("cmocka")
 if(NOT cmocka_POPULATED)
   # Fetch the content using previously declared details
