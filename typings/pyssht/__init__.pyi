4d02f1121246e059078cda903c3014eef95dd440
Mayeul d'Avezac <2745737+mdavezac@users.noreply.github.com>
2021-07-03 12:18:18 +0100

    Add stub file for pylance's sake
    Pylance doesn't like cython, apparently.

---
+++ typings/pyssht/__init__.pyi
@@ -0,0 +1,34 @@
+import numpy as np
+from typing import Optional, Tuple
+
+def forward(
+    f: np.ndarray, L: int, Spin: int = 0, Method: str = "MW", Reality: bool = False
+) -> np.ndarray:
+    pass
+
+def inverse(
+    flm: np.ndarray, L: int, Spin: int = 0, Method: str = "MW", Reality: bool = False
+) -> np.ndarray:
+    pass
+
+def elm2ind(el: int, m: int) -> int:
+    pass
+
+def ind2elm(ind: int) -> Tuple[int, int]:
+    pass
+
+def rotate_image(image, rot_list, Method=None) -> np.ndarray:
+    pass
+
+def rotate_flms(
+    f_lm: np.ndarray,
+    alpha: float,
+    beta: float,
+    gamma: float,
+    L: int,
+    dl_array_in: np.ndarray = None,
+    M_in: Optional[int] = None,
+    Axisymmetric: bool = False,
+    Keep_dl: bool = False,
+) -> np.ndarray:
+    pass
