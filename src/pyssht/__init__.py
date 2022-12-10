558bd2126dac90051070ecabb94fbe42ca580c40
Mayeul d'Avezac <2745737+mdavezac@users.noreply.github.com>
2022-12-10 13:38:13 +0000

    scikit-build: editable mode not supported

--- src/pyssht/__init__.py
+++ src/pyssht/__init__.py
@@ -4,5 +4,8 @@ from pathlib import Path
 
 __doc__ = (Path(__file__).parent / "SSHT_Python_Documentation.md").read_text()
 
-from pyssht.exceptions import ssht_spin_error, ssht_input_error  # type: ignore
+from pyssht.exceptions import (
+    ssht_spin_error as ssht_spin_error,
+    ssht_input_error as ssht_input_error,
+)
 from pyssht.cpyssht import *
